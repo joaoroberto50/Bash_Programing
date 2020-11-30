67e1a2fad7d74f832ccfdde307fc9a7c303b325c
joaoroberto50 <joaoroberto.jr35@gmail.com>
2020-11-30 15:31:52 -0300

    Update 20.11.30.2

--- src/bp_c.sh
+++ src/bp_c.sh
@@ -1,7 +1,7 @@
 bp_c(){
 	info="/*$day $hour\n\t$langs\n\t$logn in $host*/"
 
-	code="include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char *argv[]){\n\t$info \n\n\treturn 0;\n}"
+	code="#include <stdio.h>\n#include <stdlib.h>\n\nint main(int argc, char *argv[]){\n\t$info \n\n\treturn 0;\n}"
 
 	ling_ext="c"
 	
